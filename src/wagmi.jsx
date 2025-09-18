3dbe06b90c36a0fa0fc1a8df189b905efa85bc0f
aryan <aryankaushik251@gmail.com>
2025-09-18 21:47:53 +0530

    Wagmi: Added support for multiple wallets

--- src/wagmi.jsx
+++ src/wagmi.jsx
@@ -1,6 +1,6 @@
 import { http, createConfig } from "wagmi";
 import { sepolia } from "wagmi/chains";
-import { walletConnect ,injected } from "wagmi/connectors";
+import { walletConnect , metaMask, safe } from "wagmi/connectors";
 
 
 const projectId = "c103ef739c0b7fae864f5c3d70a89e3e";
@@ -9,11 +9,14 @@ export const config = createConfig({
     chains: [sepolia],
 
     connectors: [
-        injected(),
+        metaMask(),
+        safe(),
         walletConnect({projectId}),
     ],
 
     transports: {
         [sepolia.id]: http(),
     },
+
+    multiInjectedProviderDiscovery: true,
 });
