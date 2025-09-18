4960150c66c3353b0b6b3fc8185e260aca9f902a
aryan <aryankaushik251@gmail.com>
2025-09-18 16:48:13 +0530

    Wagmi.jsx: added support for walletconenct

--- src/wagmi.jsx
+++ src/wagmi.jsx
@@ -1,13 +1,16 @@
 import { http, createConfig } from "wagmi";
 import { sepolia } from "wagmi/chains";
-import { injected } from "wagmi/connectors";
+import { walletConnect ,injected } from "wagmi/connectors";
 
+
+const projectId = "c103ef739c0b7fae864f5c3d70a89e3e";
 
 export const config = createConfig({
     chains: [sepolia],
 
     connectors: [
         injected(),
+        walletConnect({projectId}),
     ],
 
     transports: {
