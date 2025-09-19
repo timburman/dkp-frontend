335305eaabb7424eac61b454f44354edec6d9970
aryan <aryankaushik251@gmail.com>
2025-09-19 21:30:26 +0530

    Added option to submit content and display submission list

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 // src/App.jsx
-
+import { useAccount } from 'wagmi';
 import { ConnectWallet } from './components/web3/ConnectWallet';
 import { ContractInfo } from './components/web3/ContractInfo';
+import { SubmissionForm } from './components/web3/SubmissionForm';
+import { SubmissionList } from './components/web3/SubmissionList';
+
 
 function App() {
+
+  const {isConnected} = useAccount();
+
   return (
-    <div className="bg-gray-900 min-h-screen flex flex-col items-center justify-center p-4">
+    <div className="bg-gray-900 min-h-screen flex flex-col items-center py-10 px-4">
       <div className="w-full max-w-md mx-auto">
         <header className="mb-8 text-center">
           <h1 className="text-4xl font-bold text-white">
@@ -14,7 +20,14 @@ function App() {
         </header>
         <main className="flex flex-col gap-6">
           <ConnectWallet />
-          <ContractInfo /> {/* 2. Add the component here */}
+          <ContractInfo />
+
+          {isConnected && (
+            <>
+              <SubmissionForm />
+              <SubmissionList /> {/* 2. Add the list here */}
+            </>
+          )}
         </main>
       </div>
     </div>
