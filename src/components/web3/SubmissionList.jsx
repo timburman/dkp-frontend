ad26192ca8195092e01a0a33b20ae13e6f6f75fe
aryan <aryankaushik251@gmail.com>
2025-09-20 15:22:58 +0530

    SubmissionList.jsx: Imported voteButtons, and changed html

--- src/components/web3/SubmissionList.jsx
+++ src/components/web3/SubmissionList.jsx
@@ -1,5 +1,6 @@
 import { useReadContract, useReadContracts } from "wagmi";
 import { DKP_CONTRACT_ABI, DKP_CONTRACT_ADDRESS } from "../../constants";
+import { VoteButtons } from "./VoteButtons";
 
 export function SubmissionList() {
 
@@ -54,19 +55,26 @@ export function SubmissionList() {
         <div className="text-white bg-gray-800 p-4 rounded-lg">
             <h2 className="text-xl font-bold mb-4">Submitted Knowledge</h2>
             <div className="flex flex-col gap-4">
-                {submissionsData && submissionsData.map((submission, index) => (
-                submission.result 
-                ? <div key={index} className="bg-gray-700 p-3 rounded break-words">
-                    <p><b>ID:</b> {submission.result[0].toString()}</p>
-                    <p><b>Hash:</b> {submission.result[1]}</p>
-                    <p><b>Author:</b> {submission.result[2]}</p>
-                    <p><b>Upvotes:</b> {submission.result[4].toString()}</p>
-                    <p><b>Downvotes:</b> {submission.result[5].toString()}</p>
+                {submissionsData && submissionsData.map((submission, index) => {
+                // Get the submission ID from the returned data (it's the first element)
+                const submissionId = submission.result[0];
+
+                return submission.result 
+                    ? <div key={index} className="bg-gray-700 p-3 rounded break-words">
+                        <p><b>ID:</b> {submission.result[0].toString()}</p>
+                        <p><b>Hash:</b> {submission.result[1]}</p>
+                        <p><b>Author:</b> {submission.result[2]}</p>
+                        <p><b>Upvotes:</b> {submission.result[4].toString()}</p>
+                        <p><b>Downvotes:</b> {submission.result[5].toString()}</p>
+                        
+                        {/* 2. Add the VoteButtons component here, passing the ID as a prop */}
+                        <VoteButtons submissionId={submissionId} />
+
                     </div>
-                : null
-                ))}
+                    : null
+                })}
             </div>
         </div>
-  );
+    );
 
 }
