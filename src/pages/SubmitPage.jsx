46a1937ce2b933759fbb91657fbf382ab8c99020
aryan <aryankaushik251@gmail.com>
2025-09-26 23:49:08 +0530

    SubmitPage.jsx: Created submit page

---
+++ src/pages/SubmitPage.jsx
@@ -0,0 +1,9 @@
+import { SubmissionForm } from "@/components/web3/SubmissionForm";
+
+export function SubmitPage() {
+    return (
+        <div>
+            <SubmissionForm />
+        </div>
+    )
+}
