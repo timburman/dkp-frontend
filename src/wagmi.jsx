e5ab06c3241959ac83972ecf6095272e2ccd6bbd
aryan <aryankaushik251@gmail.com>
2025-09-27 14:29:34 +0530

    Wagmi.jsx: Added reown's appkit for wallet connection

--- src/wagmi.jsx
+++ src/wagmi.jsx
@@ -1,22 +1,21 @@
-import { http, createConfig } from "wagmi";
-import { sepolia } from "wagmi/chains";
-import { walletConnect , metaMask, safe } from "wagmi/connectors";
-
+import { createAppKit } from "@reown/appkit/react";
+import { sepolia } from "@reown/appkit/networks";
+import { WagmiAdapter } from "@reown/appkit-adapter-wagmi";
+import { QueryClient} from "@tanstack/react-query";
 
 const projectId = "c103ef739c0b7fae864f5c3d70a89e3e";
+console.log(projectId);
 
-export const config = createConfig({
-    chains: [sepolia],
-
-    connectors: [
-        metaMask(),
-        safe(),
-        walletConnect({projectId}),
-    ],
-
-    transports: {
-        [sepolia.id]: http(),
-    },
-
-    multiInjectedProviderDiscovery: true,
+const wagmiAdapter = new WagmiAdapter({
+    networks: [sepolia],
+    projectId,
 });
+
+createAppKit({
+    adapters: [wagmiAdapter],
+    networks: [sepolia],
+    projectId,
+});
+
+export const wagmiConfig = wagmiAdapter.wagmiConfig;
+export const queryClient = new QueryClient();
