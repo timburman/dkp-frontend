1056ddac54dfabce752023051309d455c3329d82
aryan <aryankaushik251@gmail.com>
2025-09-27 14:30:06 +0530

    main.jsx: Added the new wagmi.jsx's reown implementation

--- src/main.jsx
+++ src/main.jsx
@@ -1,21 +1,22 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+// src/main.jsx
 
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
+
+// 1. Import the providers and config
 import { WagmiProvider } from 'wagmi';
-import { config } from './wagmi.jsx';
+import { QueryClientProvider } from "@tanstack/react-query";
+import { wagmiConfig, queryClient } from './wagmi.jsx';
 
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
-const queryClient = new QueryClient();
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <WagmiProvider config={config}>
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <WagmiProvider config={wagmiConfig}>
       <QueryClientProvider client={queryClient}>
         <App />
       </QueryClientProvider>
     </WagmiProvider>
-  </StrictMode>,
-)
+  </React.StrictMode>,
+);
