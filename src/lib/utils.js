c6395c72d560d7d891beb767a505ac94c5be24ad
aryan <aryankaushik251@gmail.com>
2025-10-26 19:32:21 +0530

    Utils: created helper function

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -4,3 +4,8 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs) {
   return twMerge(clsx(inputs));
 }
+
+export function shortenAddress(addr) {
+  if (!addr) return '';
+  return `${addr.substring(0, 6)}...${addr.substring(addr.length - 4)}`;
+};
