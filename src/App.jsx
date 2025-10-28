f0421c1ae87d64735e6438241e09c928ac700225
aryan <aryankaushik251@gmail.com>
2025-10-28 23:48:48 +0530

    App.jsx: Adjusted the background

--- src/App.jsx
+++ src/App.jsx
@@ -12,15 +12,15 @@ const AppLayout = () => {
   const { isConnected } = useAccount();
 
   return (
-    <div className="bg-gray-900 min-h-screen">
+    <div className="min-h-screen flex flex-col"> {/* Changed: Removed bg-gray-900 */}
       <Navbar />
-      <main>
-        <div className="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
-          {/* Outlet is a placeholder where the current page's content will be rendered */}
+      {/* Add padding-top to account for fixed navbar height */}
+      <main className="flex-grow pt-16"> {/* Changed: Added pt-16 */}
+        <div className="container mx-auto py-6 px-4 sm:px-6 lg:px-8"> {/* Changed: Use container and standard padding */}
           {isConnected ? <Outlet /> : <p className="text-white text-center">Please connect your wallet to continue.</p>}
         </div>
       </main>
-      <Toaster />
+      <Toaster /> {/* Ensure Toaster is here */}
     </div>
   );
 }
